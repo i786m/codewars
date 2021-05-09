73202d8a6b4812bd13a57ed5297ae32de4d41fe7
Imran Mohamed <ibm786@icloud.com>
2021-05-09 23:06:22 +0100

    format with

---
+++ 7 kyu/formatWith.js
@@ -0,0 +1,17 @@
+// Write
+
+// String.prototype.formatWith = function () {}
+// that takes in a list of arguments and formats the string based off these arguments and indices.
+
+// For example:
+
+// 'Hello {0}'.formatWith('Codewars') ===  'Hello Codewars'
+// '{0} + {0} = {1}'.formatWith('1','2') ===  '1 + 1 = 2'
+// 'Testing {1}'.formatWith('kata') === 'Testing {1}' // there is no arguments at position 1
+
+
+
+String.prototype.formatWith = function () {
+    return this.replace(/{\d}/g, d=>arguments[d.split('')[1]] ? arguments[d.split('')[1]] : d )
+    }
+
